a623082f15866e0f6b1bf8754d6c91a37262dad5
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-09-10 22:22:36 +0700

    Pertemuan 3-Praktikum 3

---
+++ database/seeders/SupplierSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class SupplierSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'supplier_id' => 1,
+                'supplier_kode' => 'SUP001',
+                'supplier_nama' => 'Supplier 1',
+                'supplier_alamat' => 'Jl. Merpati No.1',
+                'supplier_notlp' => '081234567890',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'supplier_id' => 2,
+                'supplier_kode' => 'SUP002',
+                'supplier_nama' => 'Supplier 2',
+                'supplier_alamat' => 'Jl. Kenari No.2',
+                'supplier_notlp' => '081234567891',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'supplier_id' => 3,
+                'supplier_kode' => 'SUP003',
+                'supplier_nama' => 'Supplier 3',
+                'supplier_alamat' => 'Jl. Elang No.3',
+                'supplier_notlp' => '081234567892',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        DB::table('m_supplier')->insert($data);
+    }
+}
