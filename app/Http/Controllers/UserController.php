4a9170b27fcc30b0aa6e26e24da8df122ae908f9
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-09-22 22:27:53 +0700

    Jobsheet 4-Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,9 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-       return view('user', ['data' => $user]);
+    {        
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
 
 
 
