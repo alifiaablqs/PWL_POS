7de0ad579cc809709b808c2e57340f287c7f8358
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-09-23 11:38:29 +0700

    Jobsheet 4-Praktikum 2.7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,6 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {        
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
-    }
-
     public function tambah()
     {
         return view ('user_tambah');
@@ -57,6 +51,12 @@ class UserController extends Controller
         $user->delete();
 
         return redirect('/user');
+    }
+
+    public function index()
+    {
+        $user = UserModel::with('level')->get();
+        return view('user', ['data' => $user]);
     }
 }
 
