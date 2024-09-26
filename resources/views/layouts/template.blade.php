74b89087d56c79d8361995648d9301dcfe416273
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-09-26 15:23:36 +0700

    Jobsheet 5-Praktikum 2

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -23,7 +23,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ assets('adminltedist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('adminltedist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -39,7 +39,7 @@
 
     <!-- Main content -->
     <section class="content">
-        @yield('content')
+      @yield('content')
 
     </section>
     <!-- /.content -->
