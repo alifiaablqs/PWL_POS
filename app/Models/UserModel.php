c6452a49f6494baa7fac6feeb1930d269a419e03
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-10-08 18:13:25 +0700

    Jobsheet 7-Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Yajra\DataTables\Html\Editor\Fields\Hidden;
+
+
 
 class UserModel extends Model
 {
@@ -13,8 +16,12 @@ class UserModel extends Model
     protected $table = 'm_user';    // mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
 
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+
+    protected $cast = ['password' => 'hashed']; // casting password agar otomatis di hash
+    
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
