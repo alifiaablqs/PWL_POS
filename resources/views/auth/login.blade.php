7ba71a374bae71c4d6bfc1805efcde273f3d0a5a
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-10-08 20:30:29 +0700

    Jobsheet 7-Tugas 4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,6 +26,16 @@ ack">
             <div class="card-header text-center"><a href="{{ url('/') }}" class="h1"><b>Admin</b>LTE</a></div>
             <div class="card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @if (session('error'))
+                    <div class="alert alert-danger">
+                        {{ session('error') }}
+                    </div>
+                @endif
                 <form action="{{ url('login') }}" method="POST" id="form-login">
                     @csrf
                     <div class="input-group mb-3">
@@ -61,6 +71,8 @@ ack">
                         <!-- /.col -->
                     </div>
                 </form>
+                <br>
+                <p>Belum punya akun? <a href="{{ url('registrasi') }}">Register</a></p>
             </div>
             <!-- /.card-body -->
         </div>
