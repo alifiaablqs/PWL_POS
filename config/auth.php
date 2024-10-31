409726327605216557f7f466968bc10d93b25199
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-10-31 19:25:11 +0700

    Jobsheet 10-Praktikum 2

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
         'api' => [
             'driver' => 'jwt',
-            'privder' => 'users',
+            'provider' => 'users',
         ],
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
