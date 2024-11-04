017cc68641a8269fae41f6fc0330a9098afd0fac
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-11-04 08:58:04 +0700

    Jobsheet 11-Praktikum 1

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -17,6 +17,7 @@ class RegisterController extends Controller
             'nama' => 'required',
             'password' => 'required|min:5|confirmed',
             'level_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
         //if validation fails
@@ -29,6 +30,7 @@ class RegisterController extends Controller
             'nama' => $request->nama,
             'password' => bcrypt($request->password),
             'level_id' => $request->level_id,
+            'image' => $request->image->hashName(),
         ]);
 
         //return response JSON user is created
@@ -38,5 +40,10 @@ class RegisterController extends Controller
                 'user' => $user,
             ], 201);
         }
+
+        //retirn JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Yajra\DataTables\Html\Editor\Fields\Hidden;
 use Illuminate\Foundation\Auth\User as Authenticable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -20,35 +20,29 @@ class UserModel extends Authenticable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return[];
+        return [];
     }
 
-    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user';        
+    protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'avatar', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+        'image'
+    ];
 
-    protected $hidden = ['password']; // jangan ditampilkan saat select
-
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
-
-    public function level(): BelongsTo
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    // Mendapatkan nama role
-    public function getRoleName(): string {
-        return $this->level->level_nama;
-    }
-
-    // Cek apakah user memiliki role tertentu
-    public function hasRole($role): bool {
-        return $this->level->level_kode == $role;
-    }
-
-    // Mendapatkan kode role
-    public function getRole() {
-        return $this->level->level_kode;
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
