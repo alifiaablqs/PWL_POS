e305d2c1932ff74a85b4d18baad560a29d895f43
Dyalifia Balqis Susanto <dyalifiabalqis22@gmail.com>
2024-11-04 09:27:46 +0700

    Revisi Jobsheet 11-Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
@@ -23,18 +23,16 @@ class UserModel extends Authenticable implements JWTSubject
         return [];
     }
 
-    protected $table = 'm_user';        
-    protected $primaryKey = 'user_id';
+    protected $table = 'm_user';        // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id';  // Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = [
-        'username',
-        'nama',
-        'password',
-        'level_id',
-        'image'
-    ];
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'avatar', 'created_at', 'updated_at', 'image'];
 
-    public function level()
+    protected $hidden = ['password']; // Jangan ditampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // Casting password agar otomatis di-hash
+
+    public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
@@ -44,5 +42,23 @@ class UserModel extends Authenticable implements JWTSubject
         return Attribute::make(
             get: fn($image) => url('/storage/posts/' . $image),
         );
+    } // Menambahkan kurung tutup untuk method image()
+
+    // Mendapatkan nama role
+    public function getRoleName(): string
+    {
+        return $this->level->level_nama;
+    }
+
+    // Cek apakah user memiliki role tertentu
+    public function hasRole($role): bool
+    {
+        return $this->level->level_kode == $role;
+    }
+
+    // Mendapatkan kode role
+    public function getRole()
+    {
+        return $this->level->level_kode;
     }
 }
